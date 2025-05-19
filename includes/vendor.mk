4149d3556e3098fcdc71815e56d6b421c6f275a3
ljbcloud <206458089+ljbcloud@users.noreply.github.com>
2025-05-19 00:34:58 +0000

    python rewrite

--- includes/vendor.mk
+++
@@ -1,6 +0,0 @@
-# this makefile is the one that should be included in every vendor/*/Makefile
-
-# this gets the directory of the last Makefile opened, which here is the current one
-SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-include $(SELF_DIR)/package.mk
